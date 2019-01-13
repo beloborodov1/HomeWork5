82bebd543129612afa34b7151647658b311b8061
unknown <C:\Users\user\AppData\Roaming\The Bat!>
2019-01-13 22:55:58 +0200

    add (for) in DoWhile.js

--- DoWhile.js
+++ DoWhile.js
@@ -1,14 +1,24 @@
-var i = 1;
-while (i < 10){
+// var i = 1;
+// while (i < 10){
+//     if (!(i%2)) break;
+//     console.log (i)
+//     i++
+// }
+
+// var b = 1;
+// while (b < 10){
+//     b++
+//     if (b % 2) continue;
+//     console.log (b)
+    
+// }
+
+for(i = 1; i < 10; i++){
     if (!(i%2)) break;
     console.log (i)
-    i++
 }
 
-var b = 1;
-while (b < 10){
-    b++
-    if (b % 2) continue;
-    console.log (b)
-    
+for(i = 1; i <= 10; i++){
+    if (!(i%2))
+    console.log(i)
 }
